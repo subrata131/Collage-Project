b7414c955c17cb1dc71e3bd601b866274c2ef437
Subrata Das <bikashdas991915@gmail.com>
2026-07-11 11:11:18 +0530

    Create student_grade_management_system.py

---
+++ Class-01/student_grade_management_system.py
@@ -0,0 +1,48 @@
+#Input Student data
+name=input("Enter Student name:")
+marks1=int(input("Enter marks for Subject 1:"))
+marks2=int(input("Enter marks for Subject 2:"))
+marks3=int(input("Enter marks for Subject 3:"))
+#For Calculatios
+total=marks1+marks2+marks3
+average=total/3
+#Grade assign Using conditional statement
+if average >=90:
+  grade='Excellent'
+
+elif average >=75:
+  grade='Good'
+elif average >=60:
+  grade='Average'
+elif average >=40:
+  grade='Bellow average'
+else:
+  grade='poor'
+# for multiple student
+
+n=int(input("Enter Number of Student:"))
+for i in range(n):
+  print("\nStudent",i+1)
+
+  name=input("Enter Student name:")
+  marks1=int(input("Enter marks for Subject 1:"))
+  marks2=int(input("Enter marks for Subject 2:"))
+  marks3=int(input("Enter marks for Subject 3:"))
+  total=marks1+marks2+marks3
+  average=total/3
+  if average >=90:
+    grade='Excellent'
+
+  elif average >=75:
+    grade='Good'
+  elif average >=60:
+   grade='Average'
+  elif average >=40:
+   grade='Bellow average'
+  else:
+   grade='poor'
+
+  print("Result:",name, "|Avg:",average, "|Grade:",grade)
+
+
+  
