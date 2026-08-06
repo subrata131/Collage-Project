2fd2b5aa80e137fd0f170f92e4c1df5e5ad2bb35
Subrata Das <bikashdas991915@gmail.com>
2026-08-06 23:40:40 +0530

    Update Email_Validation_Checker.py

--- Class-04/Email_Validation_Checker.py
+++ Class-04/Email_Validation_Checker.py
@@ -1,1 +1,23 @@
+email = input("Enter your email: ")
 
+at = 0
+dot = 0
+
+
+for ch in email:
+    if ch == "@":
+        at += 1
+    elif ch == ".":
+        dot += 1
+
+
+if at == 1 and dot >= 1:
+    at_pos = email.find("@")
+    dot_pos = email.rfind(".")
+
+    if at_pos > 0 and dot_pos > at_pos + 1 and dot_pos < len(email) - 1:
+        print("Valid Email")
+    else:
+        print("Invalid Email")
+else:
+    print("Invalid Email")
